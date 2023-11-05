d708f4eb8362fbdac6e3c695136a709a3e15878d
XirTech <john.darlucio022@gmail.com>
2023-11-06 00:41:47 +0800

    button for clear order

--- resources/views/livewire/donmono.blade.php
+++ resources/views/livewire/donmono.blade.php
@@ -8,6 +8,7 @@
                          <!-- Product 1 -->
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-3">
                    
+                       <button class="hover:bg-gray-400">
                         <div class="shadow-lg">
                             <img src="/storage/{{ $product->product_image }}"
                                 alt="Product Image" class="w-full h-60 p-3 shadow-md"> <!-- Set the height here -->
@@ -23,17 +24,10 @@
                                    <div class="order-first px-2">
                                     <p>{{ $product->product_description}}</p>
                                    </div>
-                                    
                                 </div>
-                                <div class="text-center p-2">
-                                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        <svg class="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
-                                          <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"/>
-                                        </svg>
-                                        add to cart
-                                      </button>
-                                </div>
+                              
                         </div>
+                       </button>
                   
                 </div>
                   @endforeach
