d35b8ca43ef2cfa50704bcde672f400529ad4912
XirTech <john.darlucio022@gmail.com>
2023-11-08 11:54:50 +0800

    print back to merchant

--- resources/views/livewire/receipt.blade.php
+++ resources/views/livewire/receipt.blade.php
@@ -34,6 +34,15 @@
             <button type="button" id="print" class="text-green-700  hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
                 Print
             </button>
+           @auth
+           <a href="{{ route('dashboard') }}" class="text-green-700  hover:text-white border border-none hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800">
+            Back to Merchant
+          </a>
+          @else
+          <a href="/" class="text-green-700  hover:text-white border border-none hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800">
+            Back to Merchant
+          </a>
+           @endauth
         </div>
         <h1 class="font-bold text-2xl my-4 text-center text-blue-600">KRP Services</h1>
         <hr class="mb-2">
