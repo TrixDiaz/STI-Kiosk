8e75a964926af7c415f6a7cac1a78056db6bb575
XirTech <john.darlucio022@gmail.com>
2023-11-11 10:39:21 +0800

    layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -241,7 +241,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('ippin') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/ippin.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -253,7 +253,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('kushiyaki') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/kushiyaki.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -265,7 +265,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('makizushi') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/makisushi.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -277,7 +277,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('men') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/men.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -289,7 +289,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('nigirizushi') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/nigiri.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -301,7 +301,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('ochazuke') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/ochazuke.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -313,7 +313,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('ramen') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/ramen.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -325,7 +325,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('salad') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/salad.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -337,7 +337,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('sashimi') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/sashimi.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -349,7 +349,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('tempura') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/tempura.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -361,7 +361,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('yakizakana') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/yakizakana.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -373,7 +373,7 @@
                     
                                      <!-- Product  -->
                                      <div class="p-2">
-                                        <a href="{{ route('donmono') }}">
+                                        <a href="{{ route('zensai') }}">
                                             <div class="shadow-lg">
                                                 <img src="images/zenkai.jpeg"
                                                     alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
--- resources/views/kiosk.blade.php
+++ resources/views/kiosk.blade.php
@@ -18,7 +18,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('ippin') }}">
                         <div class="shadow-lg">
                             <img src="images/ippin.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -30,7 +30,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('kushiyaki') }}">
                         <div class="shadow-lg">
                             <img src="images/kushiyaki.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -42,7 +42,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('makizushi') }}">
                         <div class="shadow-lg">
                             <img src="images/makisushi.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -54,7 +54,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('men') }}">
                         <div class="shadow-lg">
                             <img src="images/men.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -66,7 +66,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('nigirizushi') }}">
                         <div class="shadow-lg">
                             <img src="images/nigiri.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -78,7 +78,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('ochazuke') }}">
                         <div class="shadow-lg">
                             <img src="images/ochazuke.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -90,7 +90,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('ramen') }}">
                         <div class="shadow-lg">
                             <img src="images/ramen.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -102,7 +102,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('salad') }}">
                         <div class="shadow-lg">
                             <img src="images/salad.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -114,7 +114,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('sashimi') }}">
                         <div class="shadow-lg">
                             <img src="images/sashimi.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -126,7 +126,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('tempura') }}">
                         <div class="shadow-lg">
                             <img src="images/tempura.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -138,7 +138,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('yakizakana') }}">
                         <div class="shadow-lg">
                             <img src="images/yakizakana.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
@@ -150,7 +150,7 @@
 
                  <!-- Product  -->
                  <div class="p-2">
-                    <a href="{{ route('donmono') }}">
+                    <a href="{{ route('zensai') }}">
                         <div class="shadow-lg">
                             <img src="images/zenkai.jpeg"
                                 alt="Product Image" class="w-full h-60 rounded-lg shadow-lg"> <!-- Set the height here -->
