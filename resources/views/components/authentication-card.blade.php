7b5721d4dd2e28fcabca819607085fc2ab9a8a00
XirTech <john.darlucio022@gmail.com>
2023-11-11 20:48:13 +0800

    try login fix

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -3,7 +3,9 @@
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+
+    <div class="popup w-full md:w-1/2 place-content-center mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
-    </div>
+    </div>    
+
 </div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,28 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
+<head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- ReCaptcha --}}
+    <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.site_key') }}"></script>
+    @livewireStyles
+    <style>
+        /* ... your existing styles ... */
+        
+        /* Add responsive styles here */
+        @media (max-width: 768px) {
+            /* Define styles for smaller screens here */
+            .popup {
+                width: 100%;
+            }
+            .block.text-center.md\:text-left {
+                text-align: center;
+            }
+            .flex.flex-col.items-center.md\:flex-row.md\:items-center.justify-center.md\:justify-between.mt-4 {
+                justify-content: center;
+            }
+        }
+    </style>
+    
+</head>
+<body>
+    <div class="font-sans text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-
-        @livewireScripts
-    </body>
+    @stack('scripts')
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+    @livewireScripts
+</body>
 </html>
