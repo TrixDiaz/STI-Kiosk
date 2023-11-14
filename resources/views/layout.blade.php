f11c2a8ed809634fc9dab8fee630528b66c273fa
XirTech <john.darlucio022@gmail.com>
2023-11-14 10:06:04 +0800

    fixed all ogo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 9 Shopping Cart add to cart</title>
+    <title>Izakaya Shonantei</title>
+    <link rel="icon" type="image/x-icon" href="/images/iz-logo.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
