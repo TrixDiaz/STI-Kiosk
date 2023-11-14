e7e181ab75cd7ead323bda166df66f8e3c38bb7e
XirTech <john.darlucio022@gmail.com>
2023-11-14 10:02:36 +0800

    logo in all

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Izakaya Shonantei</title>
+    <link rel="icon" type="image/x-icon" href="/images/iz-logo.png">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
