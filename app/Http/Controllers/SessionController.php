474b29f7311d3353b48550de075be4501feeb1dc
XirTech <john.darlucio022@gmail.com>
2023-11-15 15:05:25 +0800

    add product name

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -55,7 +55,7 @@ class SessionController extends Controller
         ->success()
         ->icon('heroicon-o-archive-box')
         ->title('Stocks Notification')
-        ->body(Auth::user()->name . ' No stock Available for product ')
+        ->body(Auth::user()->name . ' No stock Available for product ' . $product->product_name)
         ->sendToDatabase(
             $usersToNotify =    User::whereHas('roles', function ($query) {
                 $query->where('id', [1, 2]);
