54279f5357eec2a7d4df6a216217de6f9640162b
XirTech <john.darlucio022@gmail.com>
2023-11-16 20:41:56 +0800

    pos welcome

--- resources/views/livewire/pos-page.blade.php
+++ resources/views/livewire/pos-page.blade.php
@@ -2,10 +2,10 @@
     <div class="mx-auto sm:px-6 lg:px-8">
         <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
             @foreach ($products as $product)
-                <div class="relative">
+                <div class="relative mx-10">
                     <a href="{{ route('posAddToCart', $product->id) }}">
                         <img src="/storage/{{ $product->product_image }}"
-                            class="h-40 max-w-full bg-cover rounded-lg" alt="" />
+                            class="h-40 w-full bg-cover rounded-lg" alt="" />
                         <div class="">
                             <div class="flex flex-col w-full text-black text-md text-md px-2 py-2 rounded-md">
                                 <div class="uppercase"><p class="font-thin">{{ $product->product_name }}</p></div>
