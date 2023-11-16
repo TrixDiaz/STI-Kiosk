99d73ab55e9c2c9958edd5cb98851c27beee6667
XirTech <john.darlucio022@gmail.com>
2023-11-16 18:45:57 +0800

    try full width

--- resources/views/receipt.blade.php
+++ resources/views/receipt.blade.php
@@ -901,7 +901,7 @@
         </style>
 
         <div data-aos="fade-up" data-aos-duration="2000" class="w-full text-center">
-            <div class="bg-white border rounded-lg shadow-lg px-6 py-4 max-w-2xl mx-auto">
+            <div class="bg-white border rounded-lg shadow-lg px-6 py-4 w-full mx-auto">
                 <div class="flex flex-row-reverse">
                     <button type="button" id="print"
                         class="text-green-700  hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
