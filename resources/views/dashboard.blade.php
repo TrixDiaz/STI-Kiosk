e5260839c46da4928df11b961451e77727368f29
XirTech <john.darlucio022@gmail.com>
2023-11-17 18:01:53 +0800

    tryy

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -212,7 +212,7 @@
                                             <td>
                                                 <!-- Trigger button -->
                                                 <button
-                                                    class="bg-warning hover:bg-warning text-white font-bold py-2 px-4 rounded"
+                                                    class="bg-primary hover:bg-primary-100 text-white font-bold py-2 px-4 rounded"
                                                     onclick="openModal()">
                                                     Paid
                                                 </button>
@@ -331,7 +331,7 @@
                                                                 @csrf
                                                                 @method('DELETE')
                                                                 <button id="checkoutButton"
-                                                                    class="bg-primary hover:bg-primary text-black font-bold py-2 px-4 rounded"
+                                                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                                                                     onclick="return confirm('Are you sure you want to move this order to the queue?')">
                                                                     Paid
                                                                 </button>
