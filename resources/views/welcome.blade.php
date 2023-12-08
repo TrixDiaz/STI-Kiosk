bb6e9bcf2f10d45d02c88e6814e67f8a666b9556
XirTech <john.darlucio022@gmail.com>
2023-12-09 06:50:58 +0800

    try

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -871,20 +871,11 @@
     @livewireScripts
     <script src="{{ asset('/sw.js') }}"></script>
     <script>
-        if ("serviceWorker" in navigator) {
-            // Register a service worker hosted at the root of the
-            // site using the default scope.
-            navigator.serviceWorker.register("/sw.js").then(
-                (registration) => {
-                    console.log("Service worker registration succeeded:", registration);
-                },
-                (error) => {
-                    console.error(`Service worker registration failed: ${error}`);
-                },
-            );
-        } else {
-            console.error("Service workers are not supported.");
-        }
+       if(!navigator.serviceWorker.controller) {
+           navigator.serviceWorker.register("sw.js").then(function (reg) {
+               console.log('Service worker has been registered for scope: ' + reg.scope);
+           });
+       }
     </script>
 </body>
 
