d9e0f46e67787b08cc58ee40bd17cb32a01b8cb6
XirTech <john.darlucio022@gmail.com>
2023-12-17 15:59:14 +0800

    change button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -279,8 +279,7 @@
      </span>
  </button>
     <livewire:cart-counter/>
-    <x-button class="py-0" onclick="window.location='{{ route('login') }}'">Refresh</x-button>
-
+    <button type="button" onclick="window.location='{{ route('login') }}'" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Refresh</button>
 </div>
         {{-- end of top nav --}}
         <!-- Page Content -->
