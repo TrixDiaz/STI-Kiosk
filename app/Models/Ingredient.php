2589fdc805ce5dacc102a05a2bc59d274dc3af35
XirTech <john.darlucio022@gmail.com>
2023-12-17 16:48:51 +0800

    add

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Ingredient extends Model
